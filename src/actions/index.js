b1b77499fcb3c208160fdf6d8b888e2105b2008f
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-17 10:33:17 +0800

    Define action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,23 @@
 // we'll need axios
+import axios from 'axios'
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
-
+export const LOADING = "LOADING"
+export const SUCCESS = "SUCCESS"
+export const ERROR = "ERROR"
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
-// remember that now we have controll over our thunk-based action creator
+// remember that now we have control over our thunk-based action creator
+
+export const fetchData = () => {
+  return dispatch => {
+    dispatch({ type: LOADING })
+    axios
+      .get('https://swapi.co/api/people/')
+      .then(resp => {
+        console.log(`fetchData response: `, resp.data)
+        dispatch({ type: SUCCESS, payload: resp.data.results })
+      })
+      .catch(err => dispatch({type: ERROR, payload: `Error fetching data: ${err}`}))
+  }
+}
+
